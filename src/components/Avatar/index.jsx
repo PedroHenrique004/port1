b48134fb490938bdbf7ac14afca31e853d95ece1
Pedro Santos <pedrohenriqueph004@gmail.com>
2024-01-17 19:55:48 -0300

    Estilizando a side bar

---
+++ src/components/Avatar/index.jsx
@@ -0,0 +1,7 @@
+import { FotoPerfil } from './style'
+
+const Avatar = () => (
+  <FotoPerfil src="https://github.com/PedroHenrique004.png" />
+)
+
+export default Avatar
--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,21 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+import { BotaoTema, Descricao, SideBarContainer } from './styles'
 
 const SideBar = () => (
   <aside>
-    <img src="https://github.com/PedroHenrique004.png" />
-    <Titulo fontSize={20}> Pedro Santos </Titulo>
+    <SideBarContainer>
+      <Avatar />
+      <Titulo fontSize={20}> Pedro Santos </Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        PedroHenrique004
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro front-end
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SideBarContainer>
   </aside>
 )
 
